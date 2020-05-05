3840843d218e47fed134d158587fdaeb11645cb8
Sabar <31942277+sabarop@users.noreply.github.com>
2020-05-05 23:29:42 +0700

    remove gps

--- karin-common.mk
+++ karin-common.mk
@@ -38,13 +38,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
-# GPS
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/touch_fusion.idc:system/usr/idc/touch_fusion.idc
