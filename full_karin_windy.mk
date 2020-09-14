9d4c2ba370bb2a4181845db0c46452e1aa766922
Sabar <31942277+sabarop@users.noreply.github.com>
2020-09-14 20:08:05 +0700

    fix typo

--- full_karin_windy.mk
+++ full_karin_windy.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Inherit from karin_windy device
+# Inherit from karin-common device
 $(call inherit-product, device/sony/karin_windy/device.mk)
 
 # Set those variables here to overwrite the inherited values.
