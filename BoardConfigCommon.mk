e5660cb400156a1057f9dde69fd3e0931a8c846b
Sabar <31942277+sabarop@users.noreply.github.com>
2020-10-16 23:11:33 +0700

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,9 +18,6 @@ include device/sony/kitakami-common/BoardConfigCommon.mk
 
 COMMON_DEVICE_PATH := device/sony/karin_windy
 
-# Use Snapdragon LLVM, if available
-TARGET_USE_SDCLANG := true
-
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
